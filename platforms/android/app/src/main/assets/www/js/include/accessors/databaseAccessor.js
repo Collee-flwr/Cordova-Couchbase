df1537009e8cd8c22470a8aebb838439be12aca3
Collee-flwr <campbell.colin.lee@gmail.com>
2020-12-20 13:55:28 -0600

    init

--- platforms/android/app/src/main/assets/www/js/include/accessors/databaseAccessor.js
+++ platforms/android/app/src/main/assets/www/js/include/accessors/databaseAccessor.js
@@ -2,33 +2,22 @@
 
 exports.setup = function() {
 
-    couchbase.createNewDatabase("userlogs",
-                           function(response){ document.querySelector('#message').innerHTML =response;},
-                           function(error){console.log(error);});
-
-
-
+    couchbase.createNewDatabase("userlogs", function(response){ document.querySelector('#message').innerHTML =response;},function(error){console.log(error);});
     //establish outputs
     this.output('dataOut');
+
 };
 
 exports.initialize = function() {
 
+       profile = {
+                   "name": "user"
+                 };
 
-profile = {
-                "name": "user"
+    //couchbase.insertDocument("userlogs",profile,function(response){document.querySelector('#message').innerHTML =response;},function(error){console.log(error);});
 
-          };
+    couchbase.query("userlogs",function(response){document.querySelector('#message').innerHTML =response;});
 
-
-//couchbase.insertDocument("userlogs",profile,function(response){document.querySelector('#message').innerHTML =response;},function(error){console.log(error);});
-
-
-couchbase.query("userlogs",
-                      function(response){document.querySelector('#message').innerHTML =response;}
-                    );
-
-
-this.send('dataOut');
+    this.send('dataOut');
 };
 
